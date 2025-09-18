99e3b686065c28f61d6e2162bf0555e826d49aa3
huzhen <huzhen@zetyun.com>
2025-09-18 10:49:20 +0800

    修复运行脚本

--- packages/app/src/nep_browser_engine/app.py
+++ packages/app/src/nep_browser_engine/app.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import uvicorn
+import asyncio
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
 
@@ -20,7 +21,23 @@ async def web_server_lifespan(app) -> AsyncIterator[None]:
         # Cleanup on shutdown
         await websocket_server.stop()
 
-async def main():
+
+async def run(transport: str = "stdio", port: int = 8000):
+    if transport == "stdio":
+        websocket_server = await WebSocketServer.get_instance()
+        try:
+            await websocket_server.start()
+            await mcp.run_stdio_async()
+        finally:
+            await websocket_server.stop()
+    else:
+        starlette_app = mcp.streamable_http_app()
+        starlette_app.router.lifespan_context = web_server_lifespan
+        config = uvicorn.Config(starlette_app, host="127.0.0.1", port=port)
+        server = uvicorn.Server(config)
+        await server.serve()
+
+def main():
     """Main entry point for the application."""
     parser = argparse.ArgumentParser(description="Milu MCP Server")
     parser.add_argument(
@@ -35,24 +52,13 @@ async def main():
         help="streamable-http server port"
     )
     args = parser.parse_args()
+    transport = args.transport
+    port = args.port
     if len(sys.argv) == 1 or args.transport == "stdio":
-        websocket_server = await WebSocketServer.get_instance()
-        try:
-            await websocket_server.start()
-            await mcp.run_stdio_async()
-        finally:
-            await websocket_server.stop()
-    else:
-        starlette_app = mcp.streamable_http_app()
-        starlette_app.router.lifespan_context = web_server_lifespan
-        config = uvicorn.Config(starlette_app, host="127.0.0.1", port=args.port)
-        server = uvicorn.Server(config)
-        await server.serve()
+        transport = "stdio"
+    asyncio.run(run(transport, port))
+
 
 
 if __name__ == '__main__':
-    import asyncio
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("\n检测到 Ctrl+C，正在退出...")
+    main()
